6402b089503709fd4cec619d3d5c343f91fb2a92
Denis Missias <denis@socialminer.com>
2019-07-12 12:29:52 -0300

    AspNet Identity architecture

---
+++ ByteBank.Forum/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<ApplicationUser>("DefaultConnection");
+
+                var userStore = new UserStore<ApplicationUser>(dbContext);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var newUser = new ApplicationUser
+                {
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    FullName = model.FullName
+                };
+
+                userManager.CreateAsync(newUser, model.Password);
+
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View(model);
+        }
+    }
+}
---
+++ ByteBank.Forum/ViewModels/AccountViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class AccountViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Nome completo")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+    }
+}
